13e48814e05f6eddf379441841b4116268877a1e
Yusuke Shimizu <a.ryuklnm@gmail.com>
2015-03-28 16:42:20 +0900

    世界がある

--- LifeGame.hs
+++ LifeGame.hs
@@ -26,3 +26,24 @@ neighbours (Cell x y) =
     (Cell x y) /= (Cell a b)
   ]
 
+-- | 生きているCellのリスト
+-- >>> AliveCells [Cell 0 0,  Cell 1 1,  Cell 2 3]
+-- AliveCells [Cell 0 0,Cell 1 1,Cell 2 3]
+--
+data AliveCells = AliveCells [Cell] deriving (Show)
+
+-- | 世界がある
+class World a where
+  alive :: a -> Cell -> Bool
+  nextGeneration :: a -> a
+
+-- | 世界は生きているCellを知っている(世界は無限なので死んでいるCellは見ない)
+-- >>> let world = AliveCells [Cell 1 1, Cell 2 3]
+-- >>> alive world (Cell 1 1)
+-- True
+--
+-- >>> alive world (Cell 0 0)
+-- False
+instance World AliveCells where
+  alive (AliveCells cs) p = p `elem` cs
+
